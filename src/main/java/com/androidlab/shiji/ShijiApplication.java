c17a472ad0cb56daea7dc62d38429d1ce1143e58
xmmmmmovo <13256172360@163.com>
2019-07-08 20:13:24 +0800

    fr commit

---
+++ src/main/java/com/androidlab/shiji/ShijiApplication.java
@@ -0,0 +1,13 @@
+package com.androidlab.shiji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShijiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShijiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/androidlab/shiji/ShijiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.androidlab.shiji;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShijiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
