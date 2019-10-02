0a36c6b489bd16d40e670206402cdc304fbc713a
xmmmmmovo <13256172360@163.com>
2019-10-02 18:37:40 +0800

    test failed

--- internal/dao/user_get.go
+++ internal/dao/user_get.go
@@ -8,13 +8,13 @@ import (
 )
 
 var (
-	_GetUser    = ""
+	_GetUser    = "SELECT * FROM `user`"
 	_AddUser    = "INSERT INTO `user` (`Email`, `Password`) VALUES(?, ?)"
 	_UpdateUser = ""
 )
 
 func (d *DaoStruct) GetUser(c context.Context, user *model.User) (re string, err error) {
-	err = d.db.QueryRow(c, "SELECT * FROM `user`").Scan(&re)
+	err = d.db.QueryRow(c, _GetUser).Scan(&re)
 	if err != nil && err != sql.ErrNoRows {
 		log.Error("d.GetDemo.Query error(%v)", err)
 		// 这里直接返回错误跟空re了
@@ -23,12 +23,15 @@ func (d *DaoStruct) GetUser(c context.Context, user *model.User) (re string, err
 	return re, nil
 }
 
-func (d *DaoStruct) AddUser(c context.Context, user *model.User) (err error) {
+func (d *DaoStruct) AddUser(c context.Context, user *model.User) (num int64, err error) {
 	var res sql.Result
 
 	if res, err = d.db.Exec(c, _AddUser, user.Email, user.Password); err != nil {
 		log.Error("incr user base err(%v)", err)
+		// 返回错误
 		return
 	}
-	return nil
+
+	// 这里直接返回id
+	return res.LastInsertId()
 }
